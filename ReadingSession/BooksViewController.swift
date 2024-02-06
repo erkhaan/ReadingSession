4622ba3ad787ecead63e0ad2a059e0c31ac9b8c5
erkhaan <notykt@gmail.com>
2024-02-06 23:58:15 +0600

    Add empty table view to books view controller

--- ReadingSession/BooksViewController.swift
+++ ReadingSession/BooksViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 final class BooksViewController: UIViewController {
+    
+    private var tableView = UITableView(frame: .zero, style: .plain)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
         setupNavigationItem()
+        setupLayout()
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    private func setupLayout() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
     
     private func setupNavigationItem() {
@@ -26,3 +44,17 @@ final class BooksViewController: UIViewController {
     }
 }
 
+extension BooksViewController: UITableViewDelegate {
+    
+}
+
+
+extension BooksViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+}
